400e92a0dc7259b8f9e2dc890a056f448067412a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-07-28 21:59:16 -0500

    [sm] got a ways into the back from scratch retry of the wfc tests

--- scratch.js
+++ scratch.js
@@ -1,5 +1,14 @@
 console.log("----------");
 const tiles = [];
+const grid = [];
+
+const DIM = 2;
+
+const BLANK = 0;
+const UP = 1;
+const RIGHT = 2;
+const DOWN = 3;
+const LEFT = 4;
 
 function preload() {
   tiles[0] = loadImage("/assets/tile_blank.png");
@@ -9,16 +18,62 @@ function preload() {
   tiles[4] = loadImage("/assets/tile_left.png");
 }
 
-
 function setup() {
   createCanvas(400, 400);
+
+  for (let i = 0; i < DIM * DIM; i++) {
+    grid[i] = {
+      collapsed: false,
+      options: [BLANK, UP, RIGHT, DOWN, LEFT],
+    };
+  }
+
+  grid[2].options = [BLANK, UP];
+  grid[0].options = [BLANK, LEFT];
 }
 
 function draw() {
-  background(151);
-  image(tiles[0], 0, 0, 50, 50);
-  image(tiles[1], 50, 0, 50, 50);
-  image(tiles[2], 100, 0, 50, 50);
-  image(tiles[3], 150, 0, 50, 50);
-  image(tiles[4], 0, 50, 50, 50);
+  background(0);
+
+  // Pick cell with least entropy
+  let gridCopy = grid.slice();
+  gridCopy.sort((a,b) => a.options.length - b.options.length);
+  
+  gridCopy = gridCopy.filter(cell => cell.options.length === gridCopy[0].options.length);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  const cell = random(gridCopy);
+  cell.collapsed = true;
+  const pick = random(cell.options);
+  cell.options = [pick];
+
+  console.log(grid);
+  console.log(gridCopy);
+
+  let w = width / DIM;
+  let h = height / DIM;
+  for (let j= 0; j < DIM; j++) { // row y
+    for (let i = 0; i < DIM; i++) { // column x
+      let cell = grid[i + j * DIM];
+      if (cell.collapsed) {
+        let index = cell.options[0];
+        image(tiles[index], i * w, j * h, w, h);
+    } else {
+      fill("#ff0000");
+      rect(i * w, j * h, w, h);
+    }
+  }
+  }
+
+
+  noLoop();
 }
