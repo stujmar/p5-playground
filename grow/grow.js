af8b2adf17ccfbf2d9cdcaee860c8865a49acbe6
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-07-30 21:26:04 -0500

    [sm] actually got wfc to work, still very buggy though

--- grow/grow.js
+++ grow/grow.js
@@ -1,9 +1,96 @@
+console.log("growing tree")
+
+class Cell {
+  constructor(value) {
+    this.collapsed = false;
+    if (value instanceof Array) {
+      this.options = value;
+    } else {
+      this.options = [];
+      for (let i = 0; i < value; i++) {
+        this.options[i] = i;
+      }
+    }
+  }
+}
+
+function reverseString(s) {
+  let arr = s.split('');
+  arr = arr.reverse();
+  return arr.join('');
+}
+
+function compareEdge(a, b) {
+  return a == reverseString(b);
+}
+
+class Tile {
+  constructor(img, edges, i) {
+    this.img = img;
+    this.edges = edges;
+    this.up = [];
+    this.right = [];
+    this.down = [];
+    this.left = [];
+    this.index = null;
+
+    if (i !== undefined) {
+      this.index = i;
+    }
+  }
+
+  analyze(tiles) {
+
+    // From github:
+    for (let i = 0; i < tiles.length; i++) {
+      let tile = tiles[i];
+
+      // Tile 5 can't match itself
+      if (tile.index == 5 && this.index == 5) continue;
+
+      // UP
+      if (compareEdge(tile.edges[2], this.edges[0])) {
+        this.up.push(i);
+      }
+      // RIGHT
+      if (compareEdge(tile.edges[3], this.edges[1])) {
+        this.right.push(i);
+      }
+      // DOWN
+      if (compareEdge(tile.edges[0], this.edges[2])) {
+        this.down.push(i);
+      }
+      // LEFT
+      if (compareEdge(tile.edges[1], this.edges[3])) {
+        this.left.push(i);
+      }
+    }
+}
+
+  rotate(num) {
+    const w = this.img.width;
+    const h = this.img.height;
+    const newImg = createGraphics(w, h);
+    newImg.imageMode(CENTER);
+    newImg.translate(w / 2, h / 2);
+    newImg.rotate(HALF_PI * num);
+    newImg.image(this.img, 0, 0);
+
+    const newEdges = [];
+    const len = this.edges.length;
+    for (let i = 0; i < len; i++) {
+      newEdges[i] = this.edges[(i - num + len) % len];
+    }
+    return new Tile(newImg, newEdges, this.index);
+  }
+}
+
 const tiles = [];
 const tileImages = [];
 
 let grid = [];
 
-const DIM = 20;
+const DIM = 5;
 
 function preload() {
   const path = 'assets';
@@ -22,7 +109,35 @@ function addRotation(tiles, tileIndex) {
 }
 
 function setup() {
-  createCanvas(screen.width, screen.width);
+  let element = document.querySelector("body");
+  let canvasDim = element.clientWidth < 350 ? element.clientWidth - 20 : 350;
+  createCanvas(canvasDim, canvasDim);
+  // make an html button
+  let button = document.createElement('button');
+  button.innerHTML = 'Grow';
+  button.onclick = startOver;
+  button.style.display = 'block';
+  button.style.marginLeft = "auto";
+  button.style.marginRight = "auto";
+  button.style.marginTop = "18px";
+  button.style.borderRadius = "5px";
+  button.style.backgroundColor = "#c1eefd";
+  button.style.color = "#544f42";
+  button.style.fontSize = "24px";
+  button.style.fontFamily = "Roboto, sans-serif";
+  button.style.fontWeight = "200";
+  button.style.padding = "6px 12px";
+  button.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.2)";
+  // no border on the button
+  button.style.border = "none";
+  element.appendChild(button);
+  let canvas = document.querySelector("canvas");
+  canvas.style.display = 'block';
+  canvas.style.borderRadius = "10px";
+  canvas.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.25)";
+  canvas.style.marginLeft = "auto";
+  canvas.style.marginTop = "18px";
+  canvas.style.marginRight = "auto";
   // Create tiles
   tiles[0] = new Tile(tileImages[0], ["SSS","SSS","SSS","SSS"]); // sky
   tiles[1] = new Tile(tileImages[1], ["BBB","SSS","SSS","SSS"]); // pot
@@ -50,6 +165,7 @@ function setup() {
   for (let i = 0; i < DIM * DIM; i++) {
     grid[i] = new Cell(tiles.length);
   }
+
 }
   
 
@@ -129,6 +245,18 @@ function draw() {
     return;
   }
   cell.options = [pick];
+
+  // preset one tile to collapsed
+  grid[20].collapsed = true;
+  grid[20].options = [0];
+  grid[21].collapsed = true;
+  grid[21].options = [0];
+  grid[22].collapsed = true;
+  grid[22].options = [1];
+  grid[23].collapsed = true;
+  grid[23].options = [0];
+  grid[24].collapsed = true;
+  grid[24].options = [0];
 
   const nextGrid = [];
   for (let j = 0; j < DIM; j++) {
