bafcad043381c63cf60ec008548e77966ad2e8bb
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-08-05 22:43:18 -0500

    [sm] really started to flush out the ui and settings panel

--- CC_30/cc_30.js
+++ CC_30/cc_30.js
@@ -1,4 +1,4 @@
-console.log('hello');
+console.log('.. loading phyllotaxis.js ...');
 let n = 0;
 let c = 5;
 let red = 255;
@@ -7,27 +7,39 @@ let blue = 255;
 let rate = 0;
 let go = false;
 let angle = 137.5
-let fillColor = "black";
+// let fillColor = "black";
 // turn the body of the page black
-document.body.style.background = 'green';
+document.body.style.background = 'rgb(150, 255, 150)';
 let slider = document.getElementById('angle');
 let rateSilder = document.getElementById('rate');
+let settingButton = document.getElementById('btn-settings');
+settingButton.addEventListener('click', toggleSettings);
+
+function toggleSettings() {
+  const settings = document.getElementById('settings');
+  if (settings.style.display === 'none') {
+  settings.style.display = 'block';
+  } else {
+  settings.style.display = 'none';
+  }
+}
 
 function start() {
   // get button 
-  const button = document.querySelector('button');
+  // const button = document.querySelector('#start');
+  // const settingButton = document.querySelector('#settings');
   angle = slider.value/10;
   console.log(angle, rate)
   // hide button
   // button.style.display = 'none';
   rate = rateSilder.value/1000;
-  fillColor = "yellow";
+  // fillColor = "yellow";
   go = true;
 }
 
 function setup() {
   createCanvas(400, 400);
-  background(0, 255, 0);
+  background(150, 255, 150);
   colorMode(RGB);
   angleMode(DEGREES);
   blue=255;
@@ -35,11 +47,13 @@ function setup() {
 const canvas = document.querySelector('canvas');
 // center the canvas
 // console.log(canvas)
+// green background on canvas
+canvas.style.background = 'rgb(150, 255, 150)';
 canvas.style.display = 'block';
 canvas.style.borderRadius = "100%";
 canvas.style.margin = 'auto';
 // get button
-const button = document.querySelector('button');
+const button = document.getElementById('start');
 // add event listener
 button.addEventListener('click', start);
 }
@@ -54,16 +68,11 @@ function draw() {
   let x = r * cos(a) + width / 2;
   let y = r * sin(a) + height / 2;
 
-  // console.log(red, blue);
   fill(`rgb(${red}, ${Math.floor(green)}, ${Math.floor(blue)})`);
 
-  // console.log(blue)
-  // fill('rgb(255, 255, 0)');
   blue = blue <= 0 ? 0 : blue -= .25;
   green = blue === 0 && green > 50 ? green - .25 : green;
-  // noStroke();
   ellipse(x, y, 15, 15);
-  // console.log(rate)
   n += rate;
   }
 }
