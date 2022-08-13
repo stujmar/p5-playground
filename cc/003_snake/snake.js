cb40fd28d53e24843fe146e5a3be6da7e03357b5
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-08-12 22:45:29 -0500

    [sm] got score to work, snake to wrap, death to work

--- cc/003_snake/snake.js
+++ cc/003_snake/snake.js
@@ -1,21 +1,80 @@
 function Snake() {
   this.x = 0;
   this.y = 0;
-  this.xspeed = 10;
+  this.length = 0;
+  this.tail = [];
+  this.xspeed = cell_size;
   this.yspeed = 0;
   this.dir = function(x, y) {
     this.xspeed = x;
     this.yspeed = y;
   }
 
-  this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
+  this.eat = function(food) {
+    let snakeCol = floor(this.x / cell_size);
+    let snakeRow = floor(this.y / cell_size);
+    if (snakeCol === food.x && snakeRow === food.y) {
+      score.total++;
+      this.length++;
+      food = pickLocation();
+    }
   }
 
+  this.death = function() {
+    for (var i = 0; i < this.tail.length; i++) {
+      
+      console.log("head:", this.x/cell_size, this.y/cell_size, cell_size);
+      console.log("tail:",this.tail[i].x, this.x)
+      if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+        console.log("death");
+        return true;
+      }
+    }
+  }
+
+  this.reset = function() {
+    this.x = 0;
+    this.y = 0;
+    this.length = 0;
+    this.tail = [];
+  }
+
+  this.update = function() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1]; // move all elements in the array one step forward
+    }
+    this.tail[this.length - 1] = createVector(this.x, this.y); // add a new element to the end of the array
+
+    if (this.x + this.xspeed > width - 20) {
+      this.x = 0;
+    } else if (this.x + this.xspeed < 0) {
+      this.x = width;
+    } else {
+      this.x = this.x + this.xspeed;
+    }
+
+    if (this.y + this.yspeed > height -20) {
+      this.y = 0;
+    } else if (this.y + this.yspeed < 0) {
+      this.y = height;
+    } else {
+      this.y = this.y + this.yspeed;
+    }
+
+
+    // this.x = constrain(this.x, 0, game_width - grid_size);
+    // this.y = constrain(this.y, 0, game_height - grid_size);
+
+    this.death();
+  }
+s
   this.show = function() {
+    for (let i = 0; i < this.tail.length; i++) {
+      fill(240);
+      rect(this.tail[i].x, this.tail[i].y, grid_size, grid_size);
+    }
     fill(255,255,255);
-    rect(this.x, this.y, 10, 10);
+    rect(this.x, this.y, grid_size, grid_size);
   }
 
 }
--- cc/003_snake/snakeGame.js
+++ cc/003_snake/snakeGame.js
@@ -1,10 +1,24 @@
 console.log('snake.js');
 let s;
-let game_width = 500;
-let game_height = 500;
-let grid_size = 10;
+let food;
+let score = {
+  total: 0,
+  prevScore: 0,
+  show: function() {
+    if (this.total > this.prevScore) {
+    document.getElementById('score').innerHTML = this.total;
+    }
+  }
+};
+console.log(`${window.innerWidth}x${window.innerHeight}`);
+console.log(`${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`);
+let game_width = 200;
+let game_height = 200;
+let grid_size = 20;
+let cell_size = 20;
 let grid_width = game_width / grid_size;
 let grid_height = game_height / grid_size;
+
 
 function setup() {
   let canvas = createCanvas(game_width, game_height);
@@ -14,9 +28,11 @@ function setup() {
   canvasParent.style.margin = "10px auto 0";
   canvasParent.style.width = "min-content";
   canvasParent.style.height = `${game_height}px`;
-  console.log(canvas.style);;
-  frameRate(10);
+  frameRate(9);
+  pickLocation();
+  document.getElementById('score').innerHTML = 0;
   s = new Snake();
+
 }
 
 function grid() {
@@ -30,22 +46,43 @@ function grid() {
   }
 }
 
+function pickLocation() {
+  let cols = floor(width / cell_size);
+  let rows = floor(height / cell_size);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+}
+
 function draw() {
+  // Background
   colorMode(RGB, 255);
   background(55,55,55);
   grid();
+  // Food
+  fill(240,0,100);
+  rect(food.x * cell_size, food.y * cell_size, cell_size, cell_size);
+  score.show();
+  // Snake  
   s.show();
+  s.eat(food);
   s.update();
+  if (s.death()) {
+    pickLocation();
+    s.reset();
+    score.total = 0
+  }
+
+  
 }
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -10);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 10);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(10, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-10, 0);
+  let speed = cell_size;
+  if (keyCode === UP_ARROW || keyCode === 87) {
+    s.dir(0, -cell_size);
+  } else if (keyCode === DOWN_ARROW || keyCode === 83) {
+    s.dir(0, cell_size);
+  } else if (keyCode === RIGHT_ARROW || keyCode === 68) {
+    s.dir(cell_size, 0);
+  } else if (keyCode === LEFT_ARROW || keyCode === 65) {
+    s.dir(-cell_size, 0);
   }
 }
