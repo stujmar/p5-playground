f11edc679c13a0dc8141a8b1c22e4f5135b0cf91
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-22 22:33:20 -0500

    this is the actual commit where I add width and color to the wave

--- waves/waves.js
+++ waves/waves.js
@@ -13,7 +13,8 @@ const wave = {
   length: 0.002,
   amplitude: 200,
   frequency: 0.01,
-  trails: 0.010
+  trails: 0.010,
+  lineWidth: 1
 };
 
 gui.add(wave, 'y', 0, canvas.height, 'amplitude', -200, 200);
@@ -21,21 +22,25 @@ gui.add(wave, 'length', -0.02, 0.02);
 gui.add(wave, 'frequency', 0.01, 1);
 gui.add(wave, 'amplitude', -300, 300);
 gui.add(wave, 'trails', 0.01, 1);
+gui.add(wave, 'lineWidth', 1, 10);
 
 
 
 let incement = wave.frequency;
 function animate() {
   ctx.fillStyle = `rgba(255, 255, 255, ${wave.trails})`;
+  ctx.strokeStyle = "rgb(255, 155, 155)";
+  
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath();
   ctx.moveTo(0, canvas.height/2);
-
+  
   for (let i = 0; i < canvas.width; i++) {
     if (i < canvas.width - 1 ) {
       ctx.lineTo(i, (wave.y) + Math.sin(i * wave.length + incement) * wave.amplitude);
     }
   }
+  ctx.lineWidth = wave.lineWidth;
   ctx.stroke();
   incement += wave.frequency;
   requestAnimationFrame(animate);
